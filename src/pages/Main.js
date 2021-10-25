00e70a0d569e0060b0e29b6c81389e7cdb87ea17
calvin9150 <wonbae9150@gmail.com>
2021-10-26 07:46:35 +0900

    [feat] 메인페이지 레이아웃 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,78 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
+import MainSlick from "../components/MainSlick";
+
+const Main = props => {
+  const dispatch = useDispatch();
+  const cardList = [
+    {
+      title: "삼전 풀매수 가나요?",
+      content: "내용입니다",
+      username: "김개미",
+      commentNum: 1,
+      likeNum: 11,
+    },
+    {
+      title: "삼전 풀매수 가나요?2",
+      content: "내용입니다2",
+      username: "김개미",
+      commentNum: 2,
+      likeNum: 12,
+    },
+    {
+      title: "삼전 풀매수 가나요?3",
+      content: "내용입니다3",
+      username: "김개미",
+      commentNum: 3,
+      likeNum: 13,
+    },
+    {
+      title: "삼전 풀매수 가나요?4",
+      content: "내용입니다4",
+      username: "김개미",
+      commentNum: 4,
+      likeNum: 14,
+    },
+    {
+      title: "삼전 풀매수 가나요?5",
+      content: "내용입니다5",
+      username: "김개미",
+      commentNum: 5,
+      likeNum: 15,
+    },
+  ];
+
+  return (
+    <Container>
+      <Notice>
+        <h1>개미들의 곡소리</h1>
+        <h3>고민을 올려보세요</h3>
+      </Notice>
+
+      <Wrapper height="100px" justify="end">
+        <CountContainer>
+          <span>고민 : 111개</span> <span>참여 : 111개</span>
+        </CountContainer>
+      </Wrapper>
+      <Wrapper height="300px">
+        <MainSlick cardList={cardList} type="either"></MainSlick>
+      </Wrapper>
+      <Wrapper height="300px">
+        <MainSlick cardList={cardList} type="multi"></MainSlick>
+      </Wrapper>
+    </Container>
+  );
+};
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 1280px;
-  height: 1000px;
-  background-color: royalblue;
+  height: 1200px;
+  /* background-color: royalblue; */
+  border: 1px solid gray;
   margin: auto;
 `;
 
@@ -18,33 +83,35 @@ const Notice = styled.div`
   justify-content: center;
   width: 100%;
   height: 300px;
-  background-color: wheat;
+  background-color: silver;
   h1,
   h3 {
     margin: 10px 100px;
   }
 `;
 
-const CountWrapper = styled.div``;
+const CountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 100%;
+  border: 1px solid lightgray;
 
-const SlickWrapper = styled.div`
-  width: 80%;
-  height: 300px;
-  background-color: gray;
+  span {
+    margin: 0.5em 0;
+  }
 `;
 
-const Main = props => {
-  const dispatch = useDispatch();
-
-  return (
-    <Container>
-      <Notice>
-        <h1>개미들의 곡소리</h1>
-        <h3>고민을 올려보세요</h3>
-      </Notice>
-      <SlickWrapper></SlickWrapper>
-    </Container>
-  );
-};
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: ${props => (props.justify ? props.justify : "center")};
+  width: 80%;
+  height: ${props => props.height};
+  /* background-color: gray; */
+  border: 1px solid gray;
+  margin: 50px 0 0;
+`;
 
 export default Main;
