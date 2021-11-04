0a537f99db391bf9b4a9f59f9902d036c7d8314d
larisms <larisms@gmail.com>
2021-11-04 16:11:04 +0900

    edit: childComment 삭제 뷰 적용

--- src/redux/reducers/multiDetailSlice.js
+++ src/redux/reducers/multiDetailSlice.js
@@ -3,7 +3,7 @@ import { current } from "@reduxjs/toolkit";
 
 import { DetailDB, DetailVote } from "../actions/multiDetail";
 import { AddCommentDB, DelCommentDB } from "../actions/comment";
-import { AddChildDB } from "../actions/childComment";
+import { AddChildDB, DelChildDB } from "../actions/childComment";
 
 export const initialState = {
   DetailDBLoading: false,
@@ -23,6 +23,9 @@ export const initialState = {
   AddChildDBLoading: false,
   AddChildDBDone: false,
   AddChildDBError: null,
+  DelChildDBLoading: false,
+  DelChildDBDone: false,
+  DelChildDBError: null,
 };
 
 const multiDetailSlice = createSlice({
@@ -94,10 +97,9 @@ const multiDetailSlice = createSlice({
         state.DelCommentDBDone = true;
 
         const targetIdx = state.multiDetail.comment.findIndex(p => {
-          const newTargetId = p.id;
-          return newTargetId == action.payload.newComment.id;
+          const targetId = p.id;
+          return targetId == action.payload.newComment.id;
         });
-        console.log("newTargetId", targetIdx);
 
         state.multiDetail.comment.splice(
           targetIdx,
@@ -110,8 +112,6 @@ const multiDetailSlice = createSlice({
         //   return targetId !== action.payload.newComment.id;
         // });
         // state.multiDetail.comment = [...newComment, action.payload.newComment];
-
-        console.log("current", state.multiDetail.comment);
       })
       .addCase(DelCommentDB.rejected, (state, action) => {
         state.DelCommentDBLoading = false;
@@ -127,16 +127,40 @@ const multiDetailSlice = createSlice({
       .addCase(AddChildDB.fulfilled, (state, action) => {
         state.AddChildDBLoading = false;
         state.AddChildDBDone = true;
-        state.multiDetail.childComment = [
-          ...state.multiDetail.childComment,
-          action.payload.childComment,
-        ];
-        // state.multiDetail.childComment.push(action.payload.childComment);
+        // state.multiDetail.childComment = [
+        //   ...state.multiDetail.childComment,
+        //   action.payload.childComment,
+        // ];
+        state.multiDetail.childComment.push(action.payload.childComment);
         console.log("child", action.payload);
       })
       .addCase(AddChildDB.rejected, (state, action) => {
         state.AddChildDBLoading = false;
         state.AddChildDBError = action.error;
+      })
+      //childcomment 삭제하기
+      .addCase(DelChildDB.pending, state => {
+        state.DelChildDBLoading = true;
+        state.DelChildDBDone = false;
+        state.DelChildDBError = null;
+      })
+      .addCase(DelChildDB.fulfilled, (state, action) => {
+        state.DelChildDBLoading = false;
+        state.DelChildDBDone = true;
+        const targetChildIdx = state.multiDetail.childComment.findIndex(p => {
+          const newTargetId = p.id;
+          return newTargetId == action.payload.childComment.id;
+        });
+
+        state.multiDetail.childComment.splice(
+          targetChildIdx,
+          1,
+          action.payload.childComment,
+        );
+      })
+      .addCase(DelChildDB.rejected, (state, action) => {
+        state.DelChildDBLoading = false;
+        state.DelChildDBError = action.error;
       }),
 });
 
