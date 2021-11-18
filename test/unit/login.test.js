bedd2f817d450ac0b7ba2e7ee5941456cd701557
Wonbae Jang <71538344+calvin9150@users.noreply.github.com>
2021-11-18 21:41:04 +0900

    [feat] cypress 예시 코드 추가

--- test/unit/login.test.js
+++ test/unit/login.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 
-import ErrorPage from "../../src/pages/ErrorPage";
+import { render, screen } from "../../test/testUtils";
+import Login from "../../src/pages/Login";
 
 test("login id input", () => {
-  render(<ErrorPage />);
+  render(<Login />);
   const idInput = screen.getByTestId("idInput");
   expect(idInput).toHaveTextContent("에러 페이지");
 });
